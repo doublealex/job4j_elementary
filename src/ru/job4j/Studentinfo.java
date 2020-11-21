a9760a817e0b95cc1641079f25d838e0a4a71e6e
Kazakov Alexander <doublealex92@yandex.ru>
2020-11-21 19:30:23 +0500

    3.3. Разбор первой программы. [#411226]

---
+++ src/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main (String[] args) {
+        System.out.println("Alexander Kazakov");
+        System.out.println("30.10.1992");
+    }
+}
