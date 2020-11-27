ae0d313667da3d542aa1421ead69110354b44a2a
Kazakov Alexander <doublealex92@yandex.ru>
2020-11-28 00:51:18 +0500

    0. Что такое тестирование [#411238]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -22,16 +22,28 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
+        int in1 = 140;
+        int expected1 = 2;
+        int out1 = Converter.rubleToEuro(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed1);
 
-        int rubleEuro = Converter.euroToRuble(100);
-        System.out.println("100 euro are " + rubleEuro + " rubles.");
+        int in2 = 100;
+        int expected2 = 7000;
+        int out2 = Converter.euroToRuble(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("100 euro are 700 rubles. Test result : " + passed2);
 
-        int dollar = Converter.rubleToDollar(3000);
-        System.out.println("3000 rubles are " + dollar + " dollars.");
+        int in3 = 3000;
+        int expected3 = 50;
+        int out3 = Converter.rubleToDollar(in3);
+        boolean passed3 = expected3 == out3;
+        System.out.println("3000 rubles are 50 dollars. Test result : " + passed3);
 
-        int rubleDollar = Converter.dollarToRuble(500);
-        System.out.println("500 dollars are " + rubleDollar + " rubles.");
+        int in4 = 500;
+        int expected4 = 30000;
+        int out4 = Converter.dollarToRuble(in4);
+        boolean passed4 = expected4 == out4;
+        System.out.println("500 dollars are 30000 rubles. Test result : " + passed4);
     }
 }
