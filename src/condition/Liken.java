210d18fbd7cf1da76197af3ca9e1d439e7ba517f
Kazakov Alexander <doublealex92@yandex.ru>
2020-11-29 00:08:15 +0500

    0. Операции сравнения [#411154]

---
+++ src/condition/Liken.java
@@ -0,0 +1,17 @@
+package condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 15;
+        int second = 8;
+
+        boolean result1 = first > second;
+                System.out.println(result1);
+
+        boolean result2 = first < second;
+                System.out.println(result2);
+
+        boolean result3 = first == second;
+                System.out.println(result3);
+    }
+}
