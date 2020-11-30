c8d9cf7044837b7b29899ec0a5c809bc7109c8e1
Kazakov Alexander <doublealex92@yandex.ru>
2020-12-01 00:10:56 +0500

    4.3.5. Множественное логическое выражение И [#411169]

---
+++ src/condition/Triangle.java
@@ -0,0 +1,12 @@
+package condition;
+
+public class Triangle {
+
+    public static boolean exist(double ab, double ac, double bc) {
+        return  ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(exist(3, 4, 5) ? "Exist!" : "do not exist.");
+    }
+}
