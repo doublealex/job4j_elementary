fa6657181bc1a2d665a1f6b671b29a75b9fc1683
Kazakov Alexander <doublealex92@yandex.ru>
2021-01-22 19:59:40 +0500

    1. Экзамен. Базовый синтаксис. [#411250]

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < rsl.length; i++) {
+            if (a > left.length - 1) {
+                rsl[i] = right[b];
+                b++;
+            } else if (b > right.length - 1) {
+                rsl[i] = left[a];
+                a++;
+            } else if (left[a] < right[b]) {
+                rsl[i] = left[a];
+                a++;
+            } else {
+                rsl[i] = right[b];
+                b++;
+            }
+        }
+        return rsl;
+    }
+}
